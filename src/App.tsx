50ffd3711454a2d7bc66bdfaba9dcf3af97e78a5
Алексей Хакимов <hakimowf@gmail.com>
2025-03-17 21:50:53 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MainLayout from '@layouts/MainLayout';
-import HomePage from '@pages/HomePage';
-import SynchronizationPage from '@pages/SynchronizationPage';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainLayout from "@layouts/MainLayout";
+import HomePage from "@pages/HomePage";
+import SynchronizationPage from "@pages/SynchronizationPage";
 
 const App: React.FC = () => {
   return (
@@ -11,7 +11,7 @@ const App: React.FC = () => {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/sync" element={<SynchronizationPage />} />
-          <Route path="/upload" element={<div>Загрузка</div>} />
+          <Route path="/upload" element={<div>Загрузка файлов</div>} />
         </Routes>
       </MainLayout>
     </BrowserRouter>
